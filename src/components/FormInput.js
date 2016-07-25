7d4c545352a8b8e7c3a3794f6b5d8b7bc5511abb
cm <243844647@qq.com>
2016-07-25 17:32:11 +0800

    fix formInput bug

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,62 +1,60 @@
 import React, { Component } from 'react'
-import 'styles/App.scss'
 import classNames from 'classnames'
 
 class FormInput extends Component {
 
-    constructor(props) {
-        super(props);
+    constructor(props, context) {
+        super(props, context);
+
+        this.handleFocus = this.handleFocus.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
         this.state = {
-            'inputError': false,
-            'errorText': '不能为空'
+            inputError: false,
+            errorText: '不能为空'
         };
     }
 
     handleBlur(e) {
-    	let input = e.target.value;
+    	let inputValue = e.target.value;
         if (this.props.required) {
-            if (input === '' || input === null) {
-                this.setState({ 'inputError': true });
+            if (inputValue === '' || inputValue === null) {
+                this.setState({ inputError: true });
             } else {
             	switch(this.props.type) {
             		case 'text' :
-            			this.setState({ 'inputError': false });
+            			this.setState({ inputError: false });
             			break;
             		case 'email':
-            			this.emailValid(input).bind(this);
-            			break
+            			this.emailValid(inputValue);
+            			break;
             		case 'phone':
-            			this.phoneValid(input).bind(this);
+            			this.phoneValid(inputValue);
             			break;
             		default:
-            			this.setState({ 'inputError': false });
+            			this.setState({ inputError: false });
             	}
-                this.setState({ 'inputError': false });
             }
         }
     }
 
     handleFocus() {
-        this.setState({ 'inputError': false })
+        this.setState({inputError: false })
     }
 
-   
-    emailValid(email){
-    	let reg =  /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
-    	if(reg.test(email)){
-    		this.setState({ 'inputError': false });
-    	}else{
-    		this.setState({ 'inputError': true});
-    		this.setState({'errorText':this.props.errorText});
+    emailValid(email) {
+    	const reg =  /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
+    	if(reg.test(email)) {
+    		this.setState({inputError: false});
+    	}else {
+    		this.setState({inputError: true, errorText: this.props.errorText});
     	}
     }
-    phoneValid(phone){
-    	let reg = /^0?1[3|4|5|8][0-9]\d{8}$/;
+    phoneValid(phone) {
+        const reg = /^0?1[3|4|5|8][0-9]\d{8}$/;
     	if(reg.test(phone)) {
-    		this.setState({ 'inputError': false });
+    		this.setState({inputError: false });
     	}else {
-    		this.setState({ 'inputError': true});
-    		this.setState({'errorText':this.props.errorText});
+    		this.setState({inputError: true, errorText: this.props.errorText});
     	}
 
     }
@@ -72,12 +70,12 @@ class FormInput extends Component {
         });
 
         return (
-
             <div className = 'form-input-row' >
 		        <input className = { inputClass }
-		        defaultValue = 'hello'
-		        onFocus = { this.handleFocus.bind(this) }
-		        onBlur = { this.handleBlur.bind(this) }
+		        defaultValue = ''
+		        onFocus = { this.handleFocus }
+		        onBlur = { (e) => this.handleBlur(e) }
+                placeholder = { this.props.placeholder }  
 		        /> 
 		        <p className = { pClass } > { this.state.errorText } </p>
             </div>
