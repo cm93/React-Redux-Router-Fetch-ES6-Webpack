71b1a710342f5aa02ed39e64e40e3199ba388d94
cm <243844647@qq.com>
2016-07-25 14:19:37 +0800

    update

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -5,41 +5,34 @@ import classNames from 'classnames'
 class FormInput extends Component {
 
     constructor(props) {
-        super(props)
-        console.log(props)
+        super(props);
         this.state = {
-           
             'inputError': false,
             'errorText': '不能为空'
-        }
+        };
     }
 
     handleBlur(e) {
-
-    	const input = e.target.value
+    	let input = e.target.value;
         if (this.props.required) {
             if (input === '' || input === null) {
-                this.setState({ 'inputError': true })
+                this.setState({ 'inputError': true });
             } else {
-            	;
             	switch(this.props.type) {
             		case 'text' :
-            			this.setState({ 'inputError': false })
-            			break
+            			this.setState({ 'inputError': false });
+            			break;
             		case 'email':
-            			this.emailValid(input).bind(this)
+            			this.emailValid(input).bind(this);
             			break
             		case 'phone':
-            			this.phoneValid(input).bind(this)
+            			this.phoneValid(input).bind(this);
             			break;
             		default:
-            			this.setState({ 'inputError': false })
+            			this.setState({ 'inputError': false });
             	}
-                this.setState({ 'inputError': false })
+                this.setState({ 'inputError': false });
             }
-
-        }else{
-
         }
     }
 
@@ -49,35 +42,34 @@ class FormInput extends Component {
 
    
     emailValid(email){
-    	let reg =  /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/
-    	console.log(reg.test(email));
+    	let reg =  /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
     	if(reg.test(email)){
-    		this.setState({ 'inputError': false })
+    		this.setState({ 'inputError': false });
     	}else{
-    		this.setState({ 'inputError': true})
-    		this.setState({'errorText':this.props.errorText})
+    		this.setState({ 'inputError': true});
+    		this.setState({'errorText':this.props.errorText});
     	}
     }
     phoneValid(phone){
-    	let reg = /^0?1[3|4|5|8][0-9]\d{8}$/
-    	if(reg.test(phone)){
-    		this.setState({ 'inputError': false })
-    	}else{
-    		this.setState({ 'inputError': true})
-    		this.setState({'errorText':this.props.errorText})
+    	let reg = /^0?1[3|4|5|8][0-9]\d{8}$/;
+    	if(reg.test(phone)) {
+    		this.setState({ 'inputError': false });
+    	}else {
+    		this.setState({ 'inputError': true});
+    		this.setState({'errorText':this.props.errorText});
     	}
 
     }
 
     render() {
-        const inputClass = classNames({
+        let inputClass = classNames({
             'form-input': true,
             'form-input-error': this.state.inputError
-        })
-        const pClass = classNames({
+        });
+        let pClass = classNames({
             'form-error-message': true,
             'hide': !this.state.inputError
-        })
+        });
 
         return (
 
--- src/containers/CommonContainer.js
+++ src/containers/CommonContainer.js
@@ -3,6 +3,7 @@ import React, { Component, PropTypes } from 'react'
 import Header from '../components/common/Header';
 import Login from '../components/common/Login';
 import Modal from '../components/common/Modal';
+import FormInput from '../components/FormInput';
 import {connect} from 'react-redux'
 require('../styles/yz-uikit.min.css');
 require('../styles/style.scss');
@@ -14,6 +15,8 @@ class Common extends Component {
     render() {
         return (
             <div>
+                <FormInput required={true} errorText="邮箱格式有问题" type='email' />
+
                 <Header />
 
                 <Login />
